1488a8705a4ca82ba68f8ce3fe17fd1e0f98514e
tianyamike <1974588993@qq.com>
2020-09-07 14:22:41 +0800

    执行命令添加日志

--- app/Console/Commands/CalculateActiveUser.php
+++ app/Console/Commands/CalculateActiveUser.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\Models\User;
+use Log;
 
 class CalculateActiveUser extends Command
 {
@@ -28,10 +29,12 @@ class CalculateActiveUser extends Command
      */
     public function handle(User $user)
     {
+        Log::info('Begin calculate');
         $this->info('开始计算...');
 
         $user->calculateAndCacheActiveUsers();
 
         $this->info('成功生成...');
+        Log::info('Finished');
     }
 }
