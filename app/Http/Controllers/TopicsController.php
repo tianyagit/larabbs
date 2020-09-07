bfb183bc4189d5950cef7b63b949a3fad6a8e195
tianyamike <1974588993@qq.com>
2020-09-07 11:46:20 +0800

    活跃用户

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
 use App\Models\Category;
+use App\Models\User;
 
 use Auth;
 use App\Handlers\ImageUploadHandler;
@@ -18,13 +19,14 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index(Request $request, Topic $topic)
+	public function index(Request $request, Topic $topic, User $user)
 	{
         $topics = $topic->withOrder($request->order)
                         ->with('user','category')
                         ->paginate(30);
 
-		return view('topics.index', compact('topics'));
+        $active_users = $user->getActiveUsers();
+		return view('topics.index', compact('topics', 'active_users'));
 	}
 
     public function show(Request $request, Topic $topic)
