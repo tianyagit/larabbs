2297b35a6067a0054962612df58ca05e3a4eac87
tianyamike <1974588993@qq.com>
2020-09-15 15:27:21 +0800

    短信调试

--- routes/api.php
+++ routes/api.php
@@ -3,30 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::prefix('v1')->name('api.v1.')->group(function(){
-    Route::get('version', function(){
-        abort(403, 'test');
-        return 'this is version v1';
-    })->name('version');
+
 });
 
-Route::prefix('v2')->name('api.v2.')->group(function(){
-    Route::get('version', function(){
-        return 'this is version v2';
-    })->name('version');
-});
