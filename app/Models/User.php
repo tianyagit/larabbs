066f33f385bbe808ea8b23742ac6a34f5f67ae07
tianyamike <1974588993@qq.com>
2020-09-15 17:48:02 +0800

    用户注册

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,7 +47,7 @@ class User extends Authenticatable implements MustVerifyEmailContract
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','introduction','avatar'
+        'name', 'phone', 'email', 'password','introduction','avatar'
     ];
 
     /**
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -30,5 +31,6 @@ class AppServiceProvider extends ServiceProvider
 		\App\Models\Topic::observe(\App\Observers\TopicObserver::class);
         \App\Models\Link::observe(\App\Observers\LinkObserver::class);
         //
+        JsonResource::withoutWrapping();
     }
 }
