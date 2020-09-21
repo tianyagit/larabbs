9b7940f66e6df12325de34e00619217192e56100
tianyamike <1974588993@qq.com>
2020-09-21 15:36:16 +0800

    消息通知统计

--- app/Http/Controllers/Api/NotificationsController.php
+++ app/Http/Controllers/Api/NotificationsController.php
@@ -13,4 +13,11 @@ class NotificationsController extends Controller
         $notifications = $request->user()->notifications()->paginate();
         return NotificationResource::collection($notifications);
     }
+
+    public function stats(Request $request)
+    {
+        return response()->json([
+            'unread_count' => $request->user()->notification_count
+        ]);
+    }
 }
